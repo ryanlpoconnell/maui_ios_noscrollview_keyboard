b7dac6bc9b369b0d82b27e6c7fb4a25f31d6087b
Ryan OConnell <ryan@bluesparrowapps.com>
2023-05-18 14:49:15 +0100

    Initial commit

---
+++ Scroll/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Scroll;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Scroll/MauiProgram.cs
@@ -0,0 +1,40 @@
+﻿using Maui.FixesAndWorkarounds;
+using Microsoft.Extensions.Logging;
+
+namespace Scroll;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+        //https://github.com/PureWeen/ShanedlerSamples
+		//Added to fix iOS ScrollView with Keyboard not working until .Net 8
+		//but other fixes exist as per the pick some section.
+        //builder.ConfigureMauiWorkarounds(); //configures all know workarounds
+		//Pick some...
+//        builder.ConfigureShellWorkarounds();
+//        builder.ConfigureTabbedPageWorkarounds();
+//        builder.ConfigureEntryNextWorkaround();
+//        builder.ConfigureKeyboardAutoScroll();
+//        builder.ConfigureFlyoutPageWorkarounds();
+
+//#if ANDROID
+//        builder.ConfigureEntryFocusOpensKeyboard();
+//#endif
+
+        return builder.Build();
+	}
+}
+
